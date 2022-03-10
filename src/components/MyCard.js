77af7c4db6276e168cfc395ca262efcf8ab00fdf
smk7758 <18097393+smk7758@users.noreply.github.com>
2022-03-10 10:40:27 +0900

    test

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,10 +1,10 @@
-import { useState, useRef, useContext } from 'react'
-import { RoomContext, PeerContext } from './VoiceChat';
-import icon from './image/icon.svg';
-import mic from './image/マイクアイコン.svg';
-import micoff from './image/マイク消音アイコン.svg'
-import headphone from './image/ヘッドフォンアイコン.svg';
-import officon from './image/ヘッドフォンオフアイコン.png';
+import { useState, useRef, useContext } from "react";
+import { RoomContext, PeerContext } from "./VoiceChat";
+import icon from "./image/icon.svg";
+import mic from "./image/マイクアイコン.svg";
+import micoff from "./image/マイク消音アイコン.svg";
+import headphone from "./image/ヘッドフォンアイコン.svg";
+import officon from "./image/ヘッドフォンオフアイコン.png";
 
 /*
   I, the author of this application, haven't added speaker off function
@@ -20,9 +20,13 @@ function MyCard({ localAudioRef, setUserInformation }) {
     const [status, setStatus] = useState("display");
     const myNameInputRef = useRef(null);
     const [myName, setMyName] = useState(peerRef.current.id);
+    console.log("peerRef.current.id: ", peerRef.current.id);
 
     function handleInputChange(e) {
       setMyName(e.target.value);
+      console.log(e.target.value);
+      // myName = e.target.value;
+      console.log("myName: ", myName);
       return null;
     }
 
@@ -30,12 +34,12 @@ function MyCard({ localAudioRef, setUserInformation }) {
       if (room) {
         setStatus("editting");
       } else {
-        alert("enter a room before edit your name")
+        alert("enter a room before edit your name");
       }
       //when you don't add set time out, myNameInput refers to nothing
       setTimeout(() => {
         myNameInputRef.current.focus();
-      }, 50)
+      }, 50);
       return null;
     }
 
@@ -44,7 +48,7 @@ function MyCard({ localAudioRef, setUserInformation }) {
       room.send({
         type: 2,
         name: e.target.value,
-      })
+      });
 
       setUserInformation((prev) => {
         return prev.map((user) => {
@@ -52,48 +56,50 @@ function MyCard({ localAudioRef, setUserInformation }) {
             user.name = myName;
           }
           return user;
-        })
-      })
+        });
+      });
       return null;
     }
-    function handleOnSubmitEvent(e){
+
+    function handleOnSubmitEvent(e) {
       e.preventDefault();
       setStatus("display");
       room.send({
         type: 2,
         name: e.target.value,
-      })
+      });
 
       setUserInformation((prev) => {
         return prev.map((user) => {
           if (user.id === peerRef.current.id) {
             user.name = myName;
+            console.log("setUserInformation: ", myName);
           }
           return user;
-        })
-      })
+        });
+      });
       return null;
     }
 
     return (
       <>
-        {status === "display" ?
-          <p className="my-name"
-            onDoubleClick={handleOnDoubleClickEvent}
-          >
+        {status === "display" ? (
+          <p className="my-name" onDoubleClick={handleOnDoubleClickEvent}>
             {myName}
-          </p> :
+          </p>
+        ) : (
           <form onSubmit={handleOnSubmitEvent} className="my-name-form">
-            <input value={myName}
+            <input
+              value={myName}
               onChange={handleInputChange}
               onBlur={handleOnBlurEvent}
               ref={myNameInputRef}
               className="my-name-input"
             />
           </form>
-        }
+        )}
       </>
-    )
+    );
   }
 
   return (
@@ -102,17 +108,39 @@ function MyCard({ localAudioRef, setUserInformation }) {
       <img src={icon} alt="icon" width="20%" />
       <DisplayMyName />
       {isMuted ? (
-        <img src={micoff} alt="mic-icon" width="5%" style={{ marginRight: "2%" }} onClick={() => setIsMuted(!isMuted)} />
+        <img
+          src={micoff}
+          alt="mic-icon"
+          width="5%"
+          style={{ marginRight: "2%" }}
+          onClick={() => setIsMuted(!isMuted)}
+        />
       ) : (
-        <img src={mic} alt="mic-icon" width="5%" style={{ marginRight: "2%" }} onClick={() => setIsMuted(!isMuted)} />
+        <img
+          src={mic}
+          alt="mic-icon"
+          width="5%"
+          style={{ marginRight: "2%" }}
+          onClick={() => setIsMuted(!isMuted)}
+        />
       )}
       {isSpeakerMuted ? (
-        <img src={officon} alt="speaker" width="5%" onClick={() => setIsSpeakerMuted(!isSpeakerMuted)} />
+        <img
+          src={officon}
+          alt="speaker"
+          width="5%"
+          onClick={() => setIsSpeakerMuted(!isSpeakerMuted)}
+        />
       ) : (
-        <img src={headphone} alt="speaker" width="5%" onClick={() => setIsSpeakerMuted(!isSpeakerMuted)} />
+        <img
+          src={headphone}
+          alt="speaker"
+          width="5%"
+          onClick={() => setIsSpeakerMuted(!isSpeakerMuted)}
+        />
       )}
     </div>
-  )
+  );
 }
 
 export default MyCard;
